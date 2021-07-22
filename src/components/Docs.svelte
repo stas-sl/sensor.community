188b25f6a20c2c53efd7063e304389e0831795fe
ohheyitsdave <david.lackovic@me.com>
2021-07-22 21:53:52 +0200

    :lipstick: minor guide style improvements

--- src/components/Docs.svelte
+++ src/components/Docs.svelte
@@ -102,7 +102,7 @@
                     <h2 class="flex">
                         <span class="text-brand-funcRed" id={section.slug}>
                             <a href="{section.lang}/sensors/airrohr#{section.slug}" aria-hidden="false">
-                                <svg viewBox="0 0 512 512" class="anchor">
+                                <svg viewBox="0 0 512 512" class="anchor mt-1 md:mt-0">
                                     <path fill="currentColor"
                                           d="M384,228.571v-54.857h-45.714V128h-54.857v45.714h-54.857V128h-54.857v45.714H128v54.857h45.714  v54.857H128v54.857h45.714V384h54.857v-45.714h54.857V384h54.857v-45.714H384v-54.857h-45.714v-54.857H384z M283.429,283.429  h-54.857v-54.857h54.857V283.429z"/>
                                 </svg>
@@ -113,7 +113,7 @@
                         <span class="flex justify-end space-x-8 flex-1 mt-1">
                             <a href="https://github.com/{owner}/{project}/edit/develop/content/airrohr/{$locale}/{dir}/{section.file}"
                                title="{$_('guide.editOnGithub')}">
-                                <svg class="icon text-gray-400 m-2 justify-end" viewBox='0 0 24 24' width="24"
+                                <svg class="icon text-gray-400 mt-2 justify-end" viewBox='0 0 24 24' width="24"
                                      height="24">
                                     <path d='M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34'/>
                                     <polygon points='18 2 22 6 12 16 8 16 8 12 18 2'/>
